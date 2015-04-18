6c4cf63ef5c230af920d601ed02250fd55880a3b
demiurgosoft <demiurgosoft@hotmail.com>
2015-04-18 17:15:23 +0200

    actividades con imagenes y cosas, por probar

--- php/actividades/readcontent.php
+++ php/actividades/readcontent.php
@@ -1,13 +1,19 @@
 <?php
 //partimos desde index.php
-$filename="php/actividades/content_example.txt";
-if(!file_exists($filename)) echo "file not extist";
-else{
-    //echo "FILE EXISTS";
-    $file = fopen($filename, "r") or die("Unable to open file!");
-    while(! feof($file)){
-      echo fgets($file). "<br />";
+//carga y muestra una actividad desde un fichero dado el nombre
+//de actividad y el nombre de fichero, si no se le da nombre de fichero
+//se usara php/actividades/$activityName.txt
+function loadActivity($activityname,$filename=null){
+    if(filename==null) filename="php/actividades/" . $activityname . ".txt";
+    echo "<h4>". $activityname . "</h4>"
+    if(!file_exists($filename)) echo "file not extist";
+    else{
+        $file = fopen($filename, "r") or die("Unable to open file!");
+        echo "<img src=\"" . fgets($file) "\">";
+        while(! feof($file)){
+          echo fgets($file). "<br />";
+        }
+        fclose($file);
     }
-    fclose($file);
 }
 ?>
