3af02899b71473aa8ac5233391096a587f9e70a0
sofwarejimenez <sofwarejimenezz@gmail.com>
2015-04-18 14:08:33 +0200

    read file

--- php/actividades/readcontent.php
+++ php/actividades/readcontent.php
@@ -1,22 +1,13 @@
 <?php
-#echo "<script type='text/javascript'>alert('Message has been sent');</script>";
-$filename="./content_example.txt";
+//partimos desde index.php
+$filename="php/actividades/content_example.txt";
 if(!file_exists($filename)) echo "file not extist";
 else{
-    echo "FILE EXISTS";
-    echo readfile($filename);
-    echo "<br>";
-    $myfile = fopen($filename, "r") or die("Unable to open file!");
-    //echo "<script type='text/javascript'>alert('Message has been sent');</script>";
-    echo fgets($myfile). "<br>";
+    //echo "FILE EXISTS";
+    $file = fopen($filename, "r") or die("Unable to open file!");
+    while(! feof($file)){
+      echo fgets($file). "<br />";
+    }
+    fclose($file);
 }
-/*echo "<img src=\"" . fgets($myfile) . "\">";
-echo "<p>";
-while(!feof($myfile)) {
-  echo fgets($myfile); . "<br>";
-}
-echo "</p>"
-fclose($myfile);*/
-
-
 ?>
