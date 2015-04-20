8c98f278df045efebfd20876d531de8897ce58e6
demiurgosoft <demiurgosoft@hotmail.com>
2015-04-20 12:08:23 +0200

    tabla de ponencias

--- php/ponencias.php
+++ php/ponencias.php
@@ -7,29 +7,98 @@
 			<table>
 				<thead>
 					<tr>
-						<th>Fecha</th>
 						<th>Hora</th>
-						<th>Actividad</th>
-						<th>Plazas</th>
+						<th>Dia 1</th>
+						<th>Dia 2</th>
 					</tr>
 				</thead>
 				<tr>
-					<td>dd/mm</td>
-					<td>hh:mm</td>
-					<td>Conferencia: Core dumped-¿Mortal enemigo o nuestro mejor amigo?</td>
-				<td>50</td>
+					<td>9:00-11:00</td>
+					<td>
+						<h4>ING. SOFTWARE</h4>
+						<p>
+						<b>Metodologías Ágiles</b>, por M.Noguera<br>
+						<b>IFML</b>, por M.Cabrera<br>
+						<b>Prince2</b>,por M.J.Rodríguez<br>
+						</p>
+					</td>
+					<td>
+						<h4>SISTEMA OPERATIVOS</h4>
+						<p>
+						<b>Sistemas Windows</b>, por A.León<br>
+						<b>Sistemas Unix/Linux</b>, por P.Paderewski<br>
+						<b>Sistemas IOS/Mac</b>,por R. Montes<br>
+						</p>										
+					</td>
 				</tr>
 				<tr>
-					<td>dd/mm</td>
-					<td>hh:mm</td>
-					<td>Conferencia: How to make a conference sound serious</td>
-					<td>30</td>
+					<td>11:00-11:30</td>
+					<td>Pausa-café</td>
+					<td>Pausa-café</td>
 				</tr>
 				<tr>
-					<td>dd/mm</td>
-					<td>hh:mm</td>
-					<td>Presentación del nuevo eyephone</td>
-					<td>100</td>
+					<td>11:30-13:30</td>
+					<td>
+						<h4>INFORMÁTICA GRÁFICA</h4>
+						<p>
+						<b>Visualización y Realismo</b>, por C.Ureña<br>
+						<b>Digitalización 3D</b>, por F.J.Melero :D<br>
+						<b>Realidad Aumentada</b>,por J.Revelles<br>
+						</p>
+					</td>
+					<td>
+						<h4>SISTEMA COMPLEJOS</h4>
+						<p>
+						<b>Programación Paralela</b>, por J.M. Mantas<br>
+						<b>Sistemas Distribuidos</b>, por J.L Garrido<br>
+						<b>Sistemas en Tiempo Real</b>,por J.A.Holgado<br>
+						</p>										
+					</td>
 				</tr>
-				</tbody>
+					<tr>
+						<td>13:30-115:30</td>
+						<td>Comida</td>
+						<td>Comida</td>
+					</tr>
+					<tr>
+						<td>15:00-17:00</td>
+						<td>
+							<h4>BASES DE DATOS</h4>
+							<p>
+							<b>Bases de Datos Multidimensionales</b>, por E.Garvi<br>
+							<b>Bases de datos orientadas a Objetos</b>, por J. Samos<br>
+							<b>Bases de Datos Distribuidas</b>,por C.Delgado<br>
+							</p>
+						</td>
+						<td>
+							<h4>INTERFACES DE USUARIO</h4>
+							<p>
+							<b>Interacción Háptica</b>, por F.Soler<br>
+							<b>Wearables</b>, por M.Cabrera<br>
+							<b>Realidad Virtual</b>,por J.Flores<br>
+							</p>										
+						</td>
+					</tr>
+					<tr>
+						<td>17:00-17:30</td>
+						<td>Pausa-café</td>
+						<td>Pausa-café</td>
+					</tr>
+					<tr>
+						<td>17:30-19:30</td>
+						<td>
+							<h4>COMPILADORES</h4>
+							<p>
+							<b>Procesadores de Lenguajes</b>, por J.Revelles<br>
+							<b>Traductores</b>, por R.López-Cózar<br>
+							<b>Procesaddores del habla</b>,por Z.Callejas<br>
+							</p>
+						</td>
+						<td>
+							<h4>TRABAJO FIN DE GRADO</h4>
+							<p>Mesa redonda y sesión de posters</p>										
+						</td>
+					</tr>
+				</tr>
+				<!--</tbody> -->
 			</table>
